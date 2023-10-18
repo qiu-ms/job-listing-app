1945c66b3aefa5c39143e00b28e1ce891ad636cf
Qiu, Men Seng <Men-Seng.Qiu@adesso.de>
2023-10-18 11:22:54 +0200

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Qiu, Men Seng on 18.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Qiu, Men Seng on 18.10.23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
